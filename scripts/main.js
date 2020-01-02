9bc61454c652bb521c66c862e857f10d527bf2bd
juri <jurifujii0610@gmail.com>
2020-01-03 00:06:10 +0900

    insert Ken's design

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,42 @@
-	function slideSwitch() {
-			var $active = $('#slideshow p.active');
+function slideSwitch() {
+	var $active = $('.slideshow li.active');
 
-			if ( $active.length == 0 ) $active = $('#slideshow p:last');
+	if ($active.length == 0) $active = $('.slideshow li:last');
 
-			var $next =  $active.next().length ? $active.next()
-			    : $('#slideshow p:first');
+	var $next = $active.next().length ? $active.next()
+		: $('.slideshow li:first');
 
-			$active.addClass('last-active');
+	$active.addClass('last-active');
 
-			$next.css({opacity: 0.0})
-			    .addClass('active')
-			    .animate({opacity: 1.0}, 1000, function() {
-			        $active.removeClass('active last-active');
-			    });
-		}
-
-		$(function() {
-		    setInterval( "slideSwitch()", 2100 );
+	$next.css({ opacity: 0.0 })
+		.addClass('active')
+		.animate({ opacity: 1.0 }, 1000, function () {
+			$active.removeClass('active last-active');
 		});
+}
+
+$(function () {
+	setInterval("slideSwitch()", 2100);
+});
+
+
+$(function () {
+
+	/*
+		Drawer Menu
+	------------------------------------------------*/
+	$('.js-button-hamburger').click(function () {
+		$('body').toggleClass('is-active-drawer');
+
+		if ($(this).attr('aria-expanded') == 'false') {
+			$(this).attr('aria-expanded', true);
+			// スクロールを止めるクラスを追加
+			$('body').addClass('stop-scroll');
+		} else {
+			$(this).attr('aria-expanded', false);
+			// スクロールを止めるクラスを削除
+			$('body').removeClass('stop-scroll');
+		}
+	});
+
+});
